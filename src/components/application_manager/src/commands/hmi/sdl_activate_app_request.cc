7d4bb6a6fa542caac4f20c84c0f6d0f41c2c14fe
Anton Hrytsevich <AHrytsevich@luxoft.com>
2016-05-13 15:30:39 +0300

    Uncommit previous commit

--- src/components/application_manager/src/commands/hmi/sdl_activate_app_request.cc
+++ src/components/application_manager/src/commands/hmi/sdl_activate_app_request.cc
@@ -55,12 +55,9 @@ void SDLActivateAppRequest::Run() {
       application_manager_.application(application_id);
 
   if (!app) {
-    LOG4CXX_ERROR(
+    LOG4CXX_WARN(
         logger_,
         "Can't find application within regular apps: " << application_id);
-    //APPLINK-16115
-    SendResponse(false, correlation_id(), SDL_ActivateApp, INVALID_DATA);
-    return;
   }
 
   DevicesApps devices_apps = FindAllAppOnParticularDevice(app->device());
